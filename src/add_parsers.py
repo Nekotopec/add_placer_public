11892a9fe0aef25669e80934a64447b77fc1ad95
Евгений Никольский <nekotopec@MacBook-Pro-Evgenij.local>
2019-11-25 02:12:36 +0300

    Parsing tags by name

--- src/add_parsers.py
+++ src/add_parsers.py
@@ -5,14 +5,18 @@ class EmlsParamsGetter:
     """Class for parsing add form on emls.ru."""
 
     selector_ajax = {
-        'idRegDept': ('idReg', 'https://www.emls.ru/ajax/getdept2/'),
-        'idRegDeptDist': ('idRegDept', 'https://www.emls.ru/ajax/getdist2/'),
-        'idMetro': ('idRegDept', 'https://www.emls.ru/ajax/getstation/'),
-        'idStreet': ('idRegDeptDist', 'https://www.emls.ru/ajax/getstreet2/'),
+        'id_reg_dept': ('id_reg',
+                        'https://www.emls.ru/ajax/getdept2/'),
+        'id_reg_dept_dist': ('id_reg_dept',
+                             'https://www.emls.ru/ajax/getdist2/'),
+        'id_metro': ('id_reg_dept',
+                     'https://www.emls.ru/ajax/getstation/'),
+        'id_street': ('id_reg_dept_dist',
+                      'https://www.emls.ru/ajax/getstreet2/'),
     }
 
-    select_ids = ['id_type_deal', 'idReg', 'idRegDept', 'idRegDeptDist',
-                  'idStreet', 'idMetro', 'id_amount_room', 'typeHouse',
+    select_ids = ['id_type_deal', 'id_reg', 'id_reg_dept', 'id_reg_dept_dist',
+                  'id_street', 'id_metro', 'id_amount_room', 'typeHouse',
                   'houseMaterial']
 
     # ids = ['price', 'idHouseNumber', 'sAll',
@@ -21,11 +25,12 @@ class EmlsParamsGetter:
 
     index_map = {
         'цена (тыс.руб.)': 'price',
-        'регион': 'idReg',
-        'город (район)': 'idRegDept',
-        'район города': 'idRegDeptDist',
-        '(4) Улица': 'idStreet',
-        'номер дома': 'idHouseNumber',
+        'регион': 'id_reg',
+        'город (район)': 'id_reg_dept',
+        'район города': 'id_reg_dept_dist',
+        '(4) Улица': 'id_street',
+        'метро': 'id_metro',
+        'номер дома': 'object_address_house_number',
         'кол-во комнат': 'id_amount_room',
         'общая площадь': 'sAll',
         'жилая площадь': 'sLife',
@@ -34,20 +39,21 @@ class EmlsParamsGetter:
         'высота потолка': 'sCeiling',
         'этаж': 'floor',
         'этажность': 'floorAll',
-        'тип дома': 'typeHouse',
+        'материал дома': 'typeHouse',
         'год постройки': 'yearBuild',
-        'Примечание': 'commentForClients'
+        'Примечание': 'commentForClients',
+        'тип дома': 'houseMaterial'
     }
 
-    # Сделано так, потому что нет id у данного селектора.
-    params_house_material = {
-        'не указано': '0',
-        'Кирпичный': '2',
-        'Крупно-панельн.': '3',
-        'Деревянный': '4',
-        'Блочный': '5',
-        'Монолитный': '6'
-    }
+    # Сделано так, потому что нет id у данного тега.
+    # params_house_material = {
+    #     'не указано': '0',
+    #     'Кирпичный': '2',
+    #     'Крупно-панельн.': '3',
+    #     'Деревянный': '4',
+    #     'Блочный': '5',
+    #     'Монолитный': '6'
+    # }
 
     def __init__(self, page, session):
         """
@@ -86,10 +92,10 @@ class EmlsParamsGetter:
         for key, value in self.index_map.items():
             if value in self.selector_ajax:
                 ajax_key, ajax_link = self.selector_ajax[value]
-                print(ajax_link)
-                print(ajax_key)
-                print(ajax_key in params)
-                print(params[ajax_key])
+                # print(ajax_link)
+                # print(ajax_key)
+                # print(ajax_key in params)
+                # print(params[ajax_key])
                 url = ajax_link + params[ajax_key] + '.html'
                 page = self.session.get(url)
                 lines_list = page.text.splitlines()
@@ -102,22 +108,25 @@ class EmlsParamsGetter:
 
                 if value_param:
                     params[value] = value_param
+                else:
+                    params[value] = ''
 
             elif value in self.select_ids:
                 params[value] = self._get_param(key, value, row)
 
+            elif row[key] !=row[key]:
+                print(row[key])
+                params[value] = ''
             else:
                 params[value] = row[key]
 
-        for key, value in self.params_house_material.items():
-            if row['тип дома'] in key:
-                params['houseMaterial'] = value
         return params
 
     def _get_param(self, key, param, row):
         """ Returns value from emls."""
-        tag = self.soup.find(id=param)
+        tag = self.soup.find(attrs={"name": param})
+        # print(tag)
         for child in tag.findChildren(name='option'):
-            print(child.text)
+            # print(child.text)
             if row[key] in child.text:
                 return child['value']
